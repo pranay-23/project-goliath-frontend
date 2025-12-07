93c534f9104d25ca180de04de306130d5f0dc9ac
Pranay Shah <shahpranay2305@gmail.com>
2025-12-07 22:48:44 +0530

    Update production API URL to use Render service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://project-goliath-backend.vercel.app',
+  apiUrl: 'https://project-goliath-backend.onrender.com',
   suffix: '/api',
 };
